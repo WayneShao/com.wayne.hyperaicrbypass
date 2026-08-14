6a30a021ba85200c46afbfbdc6db3ff95d04d5e5
WayneShao <owner@wayneshao.com>
2026-08-14 16:52:13 +0800

    release: prepare 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.wayne.hyperaicrbypass"
         minSdk = 28
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.3.0"
 
         buildConfigField(
             "String",
